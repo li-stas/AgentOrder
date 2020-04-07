bdbe5c4da2202b25e39872d305e9ee2986e89b70
lista <lista@bk.ru>
2020-04-07 20:54:36 +0300

    db

---
+++ src/main/resources/db/ao_add_customer.sql
@@ -0,0 +1,23 @@
+INSERT INTO AO_CUSTOMERS (Customer_nr,name,login,password,KTA,ROLE)
+VALUES (1,'admin','admin','admin',0,'ADMIN');
+
+INSERT INTO ao_customers (Customer_nr,name,login,password,KTA,ROLE)
+VALUES (51,'ПИНЧУК В.П.','login51','pwd51',51,'SUPW');
+
+INSERT INTO ao_customers (Customer_nr,name,login,password,KTA,ROLE)
+VALUES (31,'ВИТМАРК','login51','pwd31',31,'SUPW');
+
+INSERT INTO ao_customers (Customer_nr,name,login,password,KTA,ROLE)
+VALUES (364,'БУДЯК Т.И.','login364','pwd364',364,'TA');
+
+INSERT INTO ao_customers (Customer_nr,name,login,password,KTA,ROLE)
+VALUES (461,'ФИЛИАЛ ШОСТКА','login461','pwd461',461,'SUPW');
+
+INSERT INTO ao_customers (Customer_nr,name,login,password,KTA,ROLE)
+VALUES (462,'КОДЕНЕЦ А.Н.','login462','pwd462',462,'TA');
+
+-- INSERT INTO s_tag (KOD,FIO,KTAS) VALUES (469,'ГРЕК А.Ю.',31);
+-- INSERT INTO s_tag (KOD,FIO,KTAS) VALUES (535,'САВЧЕНКО И.С.',31);
+-- INSERT INTO s_tag (KOD,FIO,KTAS) VALUES (559,'КАНИВЕЦ Ю.О.',31);
+-- INSERT INTO s_tag (KOD,FIO,KTAS) VALUES (592,'БУГАЕНКО А.Ю.',31);
+-- INSERT INTO s_tag (KOD,FIO,KTAS) VALUES (594,'ЯРЕМЕНКО Е.Л.',31);
